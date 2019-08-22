c2860c8f78c68e637497a4919a20c542e0503381
leopheard <leopheard.misc@gmail.com>
2019-08-22 02:32:03 -0400

    uploads

---
+++ README.md
@@ -0,0 +1,9 @@
+Light Treason Podcast
+=============================
+
+A <a href="www.kodi.tv">KODI</a> / XBMC / Firestick audio addon for the <a href="https://www.lighttreason.news/">Light Treason Podcast</a> hosted by Allison Kilkenny, a comedy writer and former journalist currently living in NYC. Each episode, she sits down with a guest speaker to discuss current events in pop culture and politics.<br>
+
+^^^ To install this addon, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+
+<a href="https://www.lighttreason.news/"><img src="https://i1.sndcdn.com/avatars-000422752400-lcznky-original.jpg"><br>
+<br><a href="http://www.kodi.tv"><img src="https://kodi.tv/sites/default/files/page/field_image/about--devices.jpg">
---
+++ resources/lib/mainaddon.py
@@ -0,0 +1,82 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def get_soup(URL0):
+    page = requests.get(URL0)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup))
+    return soup
+get_soup("https://feeds.soundcloud.com/users/soundcloud:users:402759135/sounds.rss")
+
+def get_playable_podcast(soup):
+    subjects = []
+    for content in soup.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+#            desc = content.find('description')
+#            desc = desc.get_text()
+#            thumbnail = content.find('itunes:image')
+#            thumbnail = desc.get_text('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+#                'desc': desc,
+                'thumbnail': "https://i1.sndcdn.com/avatars-000422752400-lcznky-original.jpg",
+        }
+        subjects.append(item) 
+    return subjects
+
+def compile_playable_podcast(playable_podcast):
+    items = []
+    for podcast in playable_podcast:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+#            'info': podcast['desc'],
+            'is_playable': True,
+    })
+    return items
+
+def get_playable_podcast1(soup):
+    subjects = []
+    for content in soup.find_all('item', limit=7):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+#            desc = content.find('description')
+#            desc = desc.get_text()
+#            thumbnail = content.find('itunes:image')
+#            thumbnail = desc.get_text('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+#                'desc': desc,
+                'thumbnail': "https://i1.sndcdn.com/avatars-000422752400-lcznky-original.jpg",
+        }
+        subjects.append(item) 
+    return subjects
+
+def compile_playable_podcast1(playable_podcast1):
+    items = []
+    for podcast in playable_podcast1:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+#            'info': podcast['desc'],
+            'is_playable': True,
+    })
+    return items
